4663a72438d2f91b4ef6e6ee2fae228aef20175a
Nguyen Xuan Khuong <Nuyenxuankhuong090204@gmail.com>
2024-10-31 00:17:29 +0700

    set up

---
+++ src/quanlythuvienptit/models/DocGia.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package quanlythuvienptit.models;
+
+/**
+ *
+ * @author NguyenXuanKhuong
+ */
+public class DocGia {
+    private String SoThe, HoTen, NamSinh, GioTinh, MaNganh, MaKhoa, MaLop, NguoiCN, NgayCN;
+
+    public String getSoThe() {
+        return SoThe;
+    }
+
+    public void setSoThe(String SoThe) {
+        this.SoThe = SoThe;
+    }
+
+    public String getHoTen() {
+        return HoTen;
+    }
+
+    public void setHoTen(String HoTen) {
+        this.HoTen = HoTen;
+    }
+
+    public String getNamSinh() {
+        return NamSinh;
+    }
+
+    public void setNamSinh(String NamSinh) {
+        this.NamSinh = NamSinh;
+    }
+
+    public String getGioTinh() {
+        return GioTinh;
+    }
+
+    public void setGioTinh(String GioTinh) {
+        this.GioTinh = GioTinh;
+    }
+
+    public String getMaNganh() {
+        return MaNganh;
+    }
+
+    public void setMaNganh(String MaNganh) {
+        this.MaNganh = MaNganh;
+    }
+
+    public String getMaKhoa() {
+        return MaKhoa;
+    }
+
+    public void setMaKhoa(String MaKhoa) {
+        this.MaKhoa = MaKhoa;
+    }
+
+    public String getMaLop() {
+        return MaLop;
+    }
+
+    public void setMaLop(String MaLop) {
+        this.MaLop = MaLop;
+    }
+
+    public String getNguoiCN() {
+        return NguoiCN;
+    }
+
+    public void setNguoiCN(String NguoiCN) {
+        this.NguoiCN = NguoiCN;
+    }
+
+    public String getNgayCN() {
+        return NgayCN;
+    }
+
+    public void setNgayCN(String NgayCN) {
+        this.NgayCN = NgayCN;
+    }
+    
+    
+}
