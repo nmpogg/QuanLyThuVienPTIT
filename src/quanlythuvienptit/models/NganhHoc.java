104ca2f7f6519dc627e4d1208461be61455f674b
Akimboo <149180748+nmpogg@users.noreply.github.com>
2024-11-02 22:29:10 +0700

    models

--- src/quanlythuvienptit/models/NganhHoc.java
+++ src/quanlythuvienptit/models/NganhHoc.java
@@ -9,5 +9,32 @@ package quanlythuvienptit.models;
  * @author Admin
  */
 public class NganhHoc {
+    private String maNganhHoc;
+    private String tenNganhHoc;
+    private String maKhoa;
+
+    public void setMaNganhHoc(String maNganhHoc) {
+        this.maNganhHoc = maNganhHoc;
+    }
+
+    public void setTenNganhHoc(String tenNganhHoc) {
+        this.tenNganhHoc = tenNganhHoc;
+    }
+
+    public void setMaKhoa(String maKhoa) {
+        this.maKhoa = maKhoa;
+    }
+
+    public String getMaNganhHoc() {
+        return maNganhHoc;
+    }
+
+    public String getTenNganhHoc() {
+        return tenNganhHoc;
+    }
+
+    public String getMaKhoa() {
+        return maKhoa;
+    }
     
 }
--- src/quanlythuvienptit/models/NguoiDung.java
+++ src/quanlythuvienptit/models/NguoiDung.java
@@ -4,10 +4,87 @@
  */
 package quanlythuvienptit.models;
 
+import java.util.Date;
+
 /**
  *
  * @author Admin
  */
-public class User {
+public class NguoiDung {
+    private String username;
+    private String password;
+    private String hoTen;
+    private Date ngaySinh;
+    private String gioTinh;
+    private String chucDanh;
+    private String email;
+    private String dienThoai;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public Date getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public String getGioTinh() {
+        return gioTinh;
+    }
+
+    public String getChucDanh() {
+        return chucDanh;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDienThoai() {
+        return dienThoai;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public void setNgaySinh(Date ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public void setGioTinh(String gioTinh) {
+        this.gioTinh = gioTinh;
+    }
+
+    public void setChucDanh(String chucDanh) {
+        this.chucDanh = chucDanh;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDienThoai(String dienThoai) {
+        this.dienThoai = dienThoai;
+    }
     
+    public void ThemNguoiDung(){
+        
+    }
 }
